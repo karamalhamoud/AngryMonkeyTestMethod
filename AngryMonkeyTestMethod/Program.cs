167132a9932e7cb3d1afd459d5a41334c1469cf9
Karam Alhamoud <karam.developer@gmail.com>
2021-03-05 16:57:35 +0200

    Add project files.

---
+++ AngryMonkeyTestMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AngryMonkeyTestMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AngryMonkey.AccessTest.AccessRight[] enums = new AngryMonkey.AccessTest.AccessRight[] { AngryMonkey.AccessTest.AccessRight.Read, AngryMonkey.AccessTest.AccessRight.Update };
+            Console.WriteLine(AngryMonkey.AccessTest.HasAccess(enums, AngryMonkey.AccessTest.AccessRight.Full));
+            Console.ReadKey();
+        }
+    }
+}
