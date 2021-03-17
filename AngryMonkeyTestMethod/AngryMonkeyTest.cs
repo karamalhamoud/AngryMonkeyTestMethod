01b9f138c8c2317077d4b6e73f935afeceef18a0
Karam Alhamoud <77023145+karamalhamoud@users.noreply.github.com>
2021-03-17 13:46:50 +0200

    Update AngryMonkeyTest.cs

--- AngryMonkeyTestMethod/AngryMonkeyTest.cs
+++ AngryMonkeyTestMethod/AngryMonkeyTest.cs
@@ -1,25 +1,19 @@
-﻿using System.Linq;
+using System.Linq;
 
 namespace AngryMonkey
 {
     public class AccessTest
     {
-        public enum AccessRight
-        { Read, Create, Update, Delete, Full }
-        public static bool HasAccess(AccessRight[] userAccessRights, AccessRight requiredAccessRight)
+        public enum AccessRight { Read, Create, Update, Delete, Full }
+
+        public bool HasAccess(AccessRight[] userAccessRights, AccessRight requiredAccessRight)
         {
-            bool result = false;
-            if (requiredAccessRight == AccessRight.Full)
-                if (userAccessRights.Contains(AccessRight.Full)) result = true;
-            if (requiredAccessRight == AccessRight.Read)
-                if (userAccessRights.Contains(AccessRight.Full) || userAccessRights.Contains(AccessRight.Read)) result = true;
-            if (requiredAccessRight == AccessRight.Create)
-                if (userAccessRights.Contains(AccessRight.Full) || userAccessRights.Contains(AccessRight.Create)) result = true;
-            if (requiredAccessRight == AccessRight.Update)
-                if (userAccessRights.Contains(AccessRight.Full) || userAccessRights.Contains(AccessRight.Update)) result = true;
-            if (requiredAccessRight == AccessRight.Delete)
-                if (userAccessRights.Contains(AccessRight.Full) || userAccessRights.Contains(AccessRight.Delete)) result = true;
-            return result;
+            foreach (var right in userAccessRights)
+            {
+                if (right == requiredAccessRight)
+                   return true;
+            }
+            return false;
         }
     }
 }
